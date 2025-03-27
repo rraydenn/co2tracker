47425e098e1e5e7892e0002f5bcd6dc40665a95f
anon1 <anon1@example.com>
2025-03-27 18:46:27 +0100

    Test deploiment API adonis

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -7,9 +7,7 @@
 |
 */
 
-import { Exception } from '@adonisjs/core/exceptions'
 import router from '@adonisjs/core/services/router'
-import { STATUS_CODES } from 'http'
 
 const UsersController = () => import('#controllers/users_controller')
 
@@ -20,7 +18,6 @@ router.get('/users/me', [UsersController, 'getMyFullName'])
 router.post('/login', [UsersController, 'login'])
 
 router.post('/logout', [UsersController, 'logout'])
-
 
 router.get('/', async () => {
   return { message: 'Welcome to my API' }
