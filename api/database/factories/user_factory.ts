828171726c2e3f779679ecd429b0a5c701a5315e
anon1 <anon1@example.com>
2025-04-01 16:58:47 +0200

    modification des dossiers pour le CI/CD

--- api/database/factories/user_factory.ts
+++
@@ -1,8 +0,0 @@
-import factory from '@adonisjs/lucid/factories'
-import User from '#models/user'
-
-export const UserFactory = factory
-  .define(User, async ({ faker }) => {
-    return {}
-  })
-  .build()
