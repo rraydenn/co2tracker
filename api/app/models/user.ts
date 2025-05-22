2dc8fe49664e99b9371f70bf2ea0a31c91acdc57
anon3 <anon3@example.com>
2025-05-22 20:14:24 +0200

    chore(deploiement) re tentative pour sonarqube - tache sonarqube a ignorer

--- api/app/models/user.ts
+++ api/app/models/user.ts
@@ -5,10 +5,11 @@ import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 
-// sonarjs-ignore-next-line: S2068
+
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
-  passwordColumnName: 'password', //ColumnName not a password
+  // sonarjs-ignore-next-line: S2068
+  passwordColumnName: 'password',
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
